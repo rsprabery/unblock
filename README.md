14b4637057d4980f1fb6a0a0f0e0ec4cf9198fea
Read Sprabery <read.sprabery@gmail.com>
2013-06-14 11:23:34 -0400

    Added .idea to gitignore, added instructions to the README, added the program.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-unblock
-=======
-
+# UNBLOCK (for denyhost)
 Tool to unblock an IP address if it has been blocked by the denyhost daemon
+### Usage:
+        git clone  https://github.com/rsprabery/unblock.git
+        cd unblock
+        sudo python unblock.py IP_ADDRESS
+where `IP_ADDRESS` is the IP address that is currently blocked by denyhost.
+
+Usage is currently limited to the local machine only (which will be the use case for singly owned servers).  So if you've locked yourself out of your VPS you'll have to get access via a web-shell or similar and then run this program.
+
+## FILES MODIFIED
+#### In denyhosts folder (limited to /var/lib/denyhost at the moment)
+1. hosts
+2. hosts-restricted
+3. hosts-root
+4. hosts-valid
+5. users-hosts
+#### Other system files modified
+1. /etc/hosts.deny
---
+++ unblock.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+
+
+denyhost_files = ['/etc/hosts.deny', 'hosts', 'hosts-restricted', 'hosts-root', 'hosts-valid', 'users-hosts']
+
+
+def remove_line(text, path):
+    temp_file_path = path + ".temp"
+    read_file = file(path, 'r')
+    write_file = file(temp_file_path, 'w+')
+
+    for line in read_file.readlines():
+        if text not in line:
+            write_file.write(line)
+
+    read_file.close()
+    write_file.close()
+    os.remove(path)
+    os.rename(temp_file_path, path)
+
+
+def check_sudo():
+    flag = False
+    if os.getenv('USER') == 'root':
+        flag = True
+    return flag
+
+
+def unblock(ip_address):
+    os.chdir('/var/lib/denyhosts/')
+
+    for deny_file in denyhost_files:
+        remove_line(ip_address, deny_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Remove an IP address from DenyHost, allowing access to the machine from that IP again")
+    parser.add_argument("ip_address", help="The IP address to be removed from denyhost files", type=str)
+    args = parser.parse_args()
+
+    if not check_sudo() and args.host_addr is None:
+        print "If you want to run this locally, please run with `sudo`"
+
+    unblock(args.ip_address)
